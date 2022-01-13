2dca653b5a30a902aea34ddc015c391296541539
Vadim Melnichuk <vad.mk@ya.ru>
2022-01-13 14:43:22 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calc from './modules/calc';
 import forms from './modules/forms';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import sendForm from './modules/sendForm';
 
 timer('21:00:00 13 January 2022');
 menu();
@@ -15,3 +16,30 @@ forms('form2');
 forms('form3');
 tabs();
 slider();
+sendForm({
+  formId: 'form1',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
+sendForm({
+  formId: 'form2',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
+sendForm({
+  formId: 'form3',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
