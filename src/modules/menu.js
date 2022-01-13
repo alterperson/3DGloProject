2fc7d48683f1a33d0c7441fae0ca3188a5e61b57
Vadim Melnichuk <vad.mk@ya.ru>
2022-01-13 18:42:28 +0300

    Added fix for close-btn

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,7 +2,10 @@ const menu = () => {
   const menu = document.querySelector('menu');
 
   const handleMenu = (event) => {
-    if (event.target.closest('.menu') || event.target.className === 'close-btn' || event.target.matches('menu > ul > li > a')) {
+    if (event.target.className === 'close-btn') {
+      event.preventDefault();
+      menu.classList.toggle('active-menu');
+    } else if (event.target.closest('.menu') || event.target.matches('menu > ul > li > a')) {
       menu.classList.toggle('active-menu');
     } else if (!event.target.closest('menu')) {
       menu.classList.remove('active-menu');
